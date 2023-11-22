ce479cf7d7eb3346a69972264a4565b7681e0ac2
viktor <alycasar@gmail.com>
2023-11-22 22:53:06 +0200

    ThankYouView in views, and in urls

--- school/classroom/urls.py
+++ school/classroom/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, ThankYouView
 
 
 app_name = 'classroom'
 
 urlpatterns = [
-  path('', HomeView.as_view(), name='home')
+  path('', HomeView.as_view(), name='home'),
+  path('thank', ThankYouView.as_view(), name='thank')
 ]
--- school/classroom/views.py
+++ school/classroom/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
-# удалить
-# def home_view(request):
-#   return render(request, 'classroom/home.html')
 
 class HomeView(TemplateView):
   template_name = 'classroom/home.html'
+
+
+class ThankYouView(TemplateView):
+  template_name = 'classroom/thank.html'
