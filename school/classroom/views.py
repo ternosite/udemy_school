c95cff7bb32e85eae7a800828755757250cbd6e4
viktor <alycasar@gmail.com>
2023-11-22 22:33:46 +0200

    HomeView as TemplateView

--- school/classroom/views.py
+++ school/classroom/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
-def home_view(request):
-  return render(request, 'classroom/home.html')
+# удалить
+# def home_view(request):
+#   return render(request, 'classroom/home.html')
+
+class HomeView(TemplateView):
+  template_name = 'classroom/home.html'
